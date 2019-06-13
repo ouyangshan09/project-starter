74964298bcc696e9bdabddf16d864ac3c99bcaa2
Ouyang <ouyangshan09@gmail.com>
2019-06-13 23:00:18 +0800

    更新路由

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+import { Bussiness1 } from './components/Business1/Bussiness1.component';
 
-const App = () => <Router history={browserHistory} routes={routes} />
+const App = () => {
+    return (
+        <Router>
+            <ul className='nav-list'>
+                <li><Link to='/'>首页</Link></li>
+                <li><Link to='/about'>关于</Link></li>
+                <li><Link to='/users'>用户</Link></li>
+            </ul>
+
+            <Route path='/' exact component={Bussiness1} />
+            <Route path='/about' component={() => <div>关于路由</div>} />
+            <Route path='/users' component={() => <div>用户路由</div>} />
+        </Router>
+    )
+}
 const HotApp = hot(App);
 
 ReactDom.render(
